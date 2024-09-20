65f046109d5154c770dd9ab5c2b11db39b5dd9cc
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-09-20 12:42:21 +0100

    feat: enhance data persistence and scalability by switching to a remote postgre database

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,5 @@
 import type { PageLoad } from "./$types";
 
-const URL = "https://do-things.onrender.com";
-
 export const load: PageLoad = async ({ fetch }) => {
 	const response = await fetch(`https://do-things.onrender.com/api/v1/developers`);
 	const data = await response.json();

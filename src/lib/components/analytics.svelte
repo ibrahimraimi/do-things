867a0ccc107af1d969252b1122da880aeb0bc18b
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-09-20 13:02:12 +0100

    feat: add google gtag for analytics

---
+++ src/lib/components/analytics.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+
+	if (browser) {
+		window.dataLayer = window.dataLayer || [];
+		window.gtag = function gtag() {
+			window.dataLayer.push(arguments);
+		};
+		window.gtag("js", new Date());
+
+		window.gtag("config", "G-RFXTYCN1JG");
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-RFXTYCN1JG"></script>
+
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag("js", new Date());
+		gtag("config", PUBLIC_GA_MEASUREMENT_ID || "G-RFXTYCN1JG");
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script>
 	import "../app.pcss";
 	import Metadata from "$lib/components/metadata.svelte";
+	import Analytics from "$lib/components/analytics.svelte";
 	import SiteHeader from "$lib/components/site-header.svelte";
 </script>
 
 <Metadata />
+<Analytics />
 
 <main
 	class="mx-4 mt-4 flex max-w-2xl flex-1 flex-col antialiased md:flex-row lg:mx-auto lg:flex-col"
